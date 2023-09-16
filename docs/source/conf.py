a04388886b16bdd1414934060325179ca1f54d6e
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-09-15 20:20:57 -0600

    Update docs firts run

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@ from datetime import datetime
 
 sys.path.insert(0, os.path.abspath("../../"))
 
-from my_package import VERSION, VERSION_SHORT  # noqa: E402
+from tvKanChk import VERSION, VERSION_SHORT  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
-project = "my-package"
+project = "tvKanChk"
 copyright = f"{datetime.today().year}, Allen Institute for Artificial Intelligence"
 author = "Allen Institute for Artificial Intelligence"
 version = VERSION_SHORT
